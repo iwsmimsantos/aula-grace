6b5087f7952a81000d263c720b362e2175e2eba2
iwsmimsantos <158859354+iwsmimsantos@users.noreply.github.com>
2024-02-24 23:49:33 -0300

    up

---
+++ nome.js
@@ -0,0 +1,8 @@
+var num ;
+function numerinho(a){
+    num = a + 1;
+};
+
+//var bruna = numerinho(8);
+numerinho(8);
+console.log(num);
---
+++ pizza.js
@@ -0,0 +1,7 @@
+var num ;
+function numerinho(a, b){
+    num = (b/100) * a;
+};
+
+numerinho(300, 10);
+console.log(num);
